814c252d4550067d40f23cbec80ea4db84c1ad98
Jatin01 <145801078+jatin12j@users.noreply.github.com>
2026-01-05 12:29:52 +0530

    Time: 0 ms (100%), Space: 39.2 MB (25.94%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -4,19 +4,19 @@ public:
         long long sum = 0;
         int c = 0;
         int mini = INT_MAX;
-        int n = matrix.size();
+        int n =matrix.size();
         int m = matrix[0].size();
         
         for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
+              for(int j=0; j<m; j++) {
                 sum += abs(matrix[i][j]);
-                if(matrix[i][j] < 0) c++;
+                   if(matrix[i][j] < 0) c++;
 
-                mini = min(mini, abs(matrix[i][j]));
+                  mini = min(mini, abs(matrix[i][j]));
             }
         }
         
-        if(c % 2) sum -= 2*mini;
+         if(c % 2) sum -= 2*mini;
         
         return sum;
     }
