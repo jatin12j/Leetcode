220fe84b29b2aafa1e756932f92bcb94f5671f46
Jatin01 <145801078+jatin12j@users.noreply.github.com>
2026-01-23 11:19:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (66.29%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-
         if(x<0)
-            return false;
-        
-        long long rem,ans=0,num=x;
+        return false;
+
+        long long rem;
+        long long ans=0;
+        long long num=x;
+
         while(num!=0){
             rem = num%10;
-            num/=10;
+            num /=10;
             ans = ans*10+rem;
         }
-        return (ans==x);
+
+        return {ans==x};
     }
 };
