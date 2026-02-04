51ecbe010214a6ccfe5af58151ee51bb38ab452b
Jatin01 <145801078+jatin12j@users.noreply.github.com>
2026-02-04 12:56:49 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.14%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-
         for(char c:s){
             if(c=='(' || c=='{' || c=='[') st.push(c);
             else{
                 if(st.empty()) return false;
 
-                if((c==')' && st.top()!='(')|| 
+                if((c==')' && st.top()!='(') ||
                   (c=='}' && st.top()!='{') ||
                   (c==']' && st.top()!='['))
                   return false;
                   st.pop();
             }
         }
-       return  st.empty();
+        return st.empty();
     }
 };
