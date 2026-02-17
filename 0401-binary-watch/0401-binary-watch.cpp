c1ceff1209b587d419c794eb36a4500f30640430
Jatin01 <145801078+jatin12j@users.noreply.github.com>
2026-02-17 21:26:40 +0530

    Time: 0 ms (100%), Space: 9.7 MB (65.38%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.cpp
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -3,23 +3,22 @@ public:
     vector<string> readBinaryWatch(int turnedOn) {
         vector<string> res;
 
-        for(int i=0;i<12;i++){
-            for(int j=0;j<60;j++){
+        for(int hour = 0; hour < 12; hour++ ){
+            for(int minute = 0; minute < 60; minute++){
+                if(__builtin_popcount(hour) + __builtin_popcount(minute) == turnedOn){
 
-                if(__builtin_popcount(i) + __builtin_popcount(j) == turnedOn){
+                    string time = to_string(hour) + ":";
 
-                    string time = to_string(i) + ":";
-
-                    if(j<10){
+                    if(minute<10){
                         time += "0";
                     }
 
-                    time += to_string(j);
+                    time += to_string(minute);
 
                     res.push_back(time);
                 }
             }
         }
-        return res;
+       return res; 
     }
 };
