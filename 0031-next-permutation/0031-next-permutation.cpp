dfa249d9bfbbec437220c510fb42b228193851df
Jatin01 <145801078+jatin12j@users.noreply.github.com>
2026-07-10 10:42:22 +0530

    Time: 0 ms (100%), Space: 15.6 MB (71.73%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        //finding main element which gola_ele
+        int gola_index=-1;
+
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                gola_index = i-1;
+                break;
+            }
+        }
+        if(gola_index!=-1){
+        int swap_index = gola_index;
+        for(int j=n-1;j>=gola_index+1;j--){
+            if(nums[j]>nums[swap_index]){
+                swap_index=j;
+                break;
+            }    
+        }
+        swap(nums[swap_index],nums[gola_index]);
+        }
+        reverse(nums.begin()+gola_index+1,nums.end());
+    }
+};
