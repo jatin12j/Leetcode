12b4614454a38033787765dba773f92bc2b7958c
Jatin01 <145801078+jatin12j@users.noreply.github.com>
2026-07-26 12:10:35 +0530

    Time: 11 ms (37.37%), Space: 31.5 MB (76.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -4,7 +4,6 @@ public:
         int n=nums.size();
         sort(nums.begin(),nums.end());
 
-        return max(nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[n-1]);
-        
+        return max(nums[n-3]*nums[n-2]*nums[n-1],nums[0]*nums[1]*nums[n-1]);
     }
 };
