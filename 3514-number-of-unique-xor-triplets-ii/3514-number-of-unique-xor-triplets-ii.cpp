285078719034fe2b6d4a060bd707a0a35df8b679
Jatin01 <145801078+jatin12j@users.noreply.github.com>
2026-07-26 07:33:36 +0530

    Time: 2033 ms (26.83%), Space: 98.4 MB (26.42%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n=nums.size();
-
-        //store all XOR pair
         unordered_set<int>s1;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
@@ -11,14 +9,12 @@ public:
             }
         }
 
-    //again find triplet XOR with above
-    unordered_set<int>s2;
-    for(int pairs : s1){
-        for(int &num : nums){
-            s2.insert(pairs ^ num);
+        unordered_set<int>s2;
+        for(int pairs : s1){
+            for(int &num : nums){
+                s2.insert(pairs^num);
+            }
         }
-    }
         return s2.size();
-        
     }
 };
