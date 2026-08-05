82b33f6d454f4909bd1768b347a43d40a9b4e291
Jatin01 <145801078+jatin12j@users.noreply.github.com>
2026-08-05 17:35:13 +0530

    Time: 11 ms (22.66%), Space: 11.7 MB (28.81%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-   int t[501][501];
-    int solve(int i,int j, vector<int>& piles){
+    int t[501][501];
+    int solve(int i,int j,vector<int>& piles){
         if(i>j) return 0;
         if(t[i][j]!=-1) return t[i][j];
-        int take_i = piles[i] + min(solve(i+2,j,piles),solve(i+1,j-1,piles));
-        int take_j = piles[j] + min(solve(i,j-2,piles),solve(i+1,j-1,piles));
+        int take_i = piles[i]+ min(solve(i+2,j,piles),solve(i+1,j-1,piles));
+        int take_j = piles[j]+min(solve(i+1,j-1,piles),solve(i,j-2,piles));
         return t[i][j]=max(take_i,take_j);
     }
     bool stoneGame(vector<int>& piles) {
-      int n=piles.size();
-      memset(t,-1,sizeof(t));
-      int sum = accumulate(begin(piles),end(piles),0);
-      int first = solve(0,n-1,piles);
-      return first >sum/2;
+        int n=piles.size();
+        memset(t,-1,sizeof(t));
+        int tot = accumulate(begin(piles),end(piles),0);
+        int first = solve(0,n-1,piles);
+        return first>tot/2;
     }
 };
