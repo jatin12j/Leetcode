72492aeec21a40953a002cc7d6bd9a6685f32a48
Jatin01 <145801078+jatin12j@users.noreply.github.com>
2026-08-27 00:02:28 +0530

    Time: 0 ms (100%), Space: 19.4 MB (65.41%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            long long rem = target - nums[i];
-            if(mp.find(rem)!=mp.end()){
-                return {mp[rem]+1,i+1};
-            }
-            mp[nums[i]]=i;
+        //-------we can use this also as 2 sum 
+    //     int n=nums.size();
+    //     unordered_map<int,int>mp;
+    //     for(int i=0;i<n;i++){
+    //         long long rem = target - nums[i];
+    //         if(mp.find(rem)!=mp.end()){
+    //             return {mp[rem]+1,i+1};
+    //         }
+    //         mp[nums[i]]=i;
+    //     }
+    //     return {};
+
+
+    // new idea is so array is sorted so we have to use 2 pointer
+    int n=nums.size();
+    int left=0;
+    int right= n-1;
+    while(left<right){
+        int sum = nums[left]+nums[right];
+        if(sum==target){
+            return {left+1,right+1};
         }
-        return {};
-    }
+        else if(sum>target){
+            right--;
+        }else{
+            left++;
+        }
+            }
+            return {};
+     }
 };
