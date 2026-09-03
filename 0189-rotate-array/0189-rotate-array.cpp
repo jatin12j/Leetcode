b35a68cec1a5ce1cbf862ed222dd67b2ae18ca50
Jatin01 <145801078+jatin12j@users.noreply.github.com>
2026-09-03 15:38:36 +0530

    Time: 15 ms (5.03%), Space: 262.5 MB (34.89%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-         k=k%n; // reducing recycle
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
-       
+        k=k%n;
+        vector<int>ans;
+        for(int i=n-k;i<n;i++){
+            ans.push_back(nums[i]);
+        }
+        for(int i=0;i<n-k;i++){
+            ans.push_back(nums[i]);
+        }
+        nums=ans;
     }
 };
